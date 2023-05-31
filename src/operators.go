8c999b27d4c8c469b23b11d02424bfa53e1ff3a9
Daniel Méndez <daphni_mgld@hotmail.com>
2023-05-30 22:55:52 -0600

    feat: Operators

---
+++ src/operators.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	y := 25
+
+	// addition
+	fmt.Println(x + y) // 35
+
+	// substraction
+	fmt.Println(x - y) // -15
+
+	// multiplication
+	fmt.Println(x * y) // 250
+
+	// division
+	fmt.Println(x / y) // .4
+	
+	// module
+	fmt.Println(x % y) // 0
+
+	// incremental
+	x++
+	fmt.Println(x) // 11
+
+	// decremental
+	y--
+	fmt.Println(y) // 24
+	
+
+}
