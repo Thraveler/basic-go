a3640423273bfe97217ab63777773e0e80041042
Daniel Méndez <daphni_mgld@hotmail.com>
2023-05-30 22:44:40 -0600

    feat: Variables and constants declaration

--- src/main.go
+++ src/main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+	// Constant declaration
+	const piNumber float64 = 3.1416
+	const name = "Daniel"
+
+	fmt.Println(piNumber, name)
+
+	// Variables
+	age := 12
+	var founds = 1500.34
+	// var heigth float32
+
+	fmt.Println("Age: ", age)
+	fmt.Println("Founds: ", founds)
+	// fmt.Println("Age: ", age)
+
+	// Zero values
+	var a int
+	var b float32
+	var c string
+	var d bool
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
 }
